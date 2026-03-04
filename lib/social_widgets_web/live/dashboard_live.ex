dc77d9535bd5097fc4ffe2732a6174f3949fed3a
Fly Dev <dev@fly.io>
2026-03-04 11:18:57 +0000

    add poll question and options to creation form

--- lib/social_widgets_web/live/dashboard_live.ex
+++ lib/social_widgets_web/live/dashboard_live.ex
@@ -1,6 +1,6 @@
 defmodule SocialWidgetsWeb.DashboardLive do
   use SocialWidgetsWeb, :live_view
-  alias SocialWidgets.Widgets
+  alias SocialWidgets.{Widgets, Polls}
 
   @impl true
   def mount(_params, _session, socket) do
@@ -15,12 +15,18 @@ defmodule SocialWidgetsWeb.DashboardLive do
      |> assign(:widgets, widgets)
      |> assign(:widget_form, to_form(Widgets.change_widget(%Widgets.Widget{})))
      |> assign(:show_form, false)
-     |> assign(:selected_widget, nil)}
+     |> assign(:selected_widget, nil)
+     |> assign(:poll_question, "")
+     |> assign(:poll_options, ["", "", "", ""])}
   end
 
   @impl true
   def handle_event("toggle_form", _params, socket) do
-    {:noreply, assign(socket, :show_form, !socket.assigns.show_form)}
+    {:noreply,
+     socket
+     |> assign(:show_form, !socket.assigns.show_form)
+     |> assign(:poll_question, "")
+     |> assign(:poll_options, ["", "", "", ""])}
   end
 
   @impl true
@@ -34,21 +40,88 @@ defmodule SocialWidgetsWeb.DashboardLive do
   end
 
   @impl true
-  def handle_event("save", %{"widget" => widget_params}, socket) do
-    case Widgets.create_widget(widget_params) do
-      {:ok, widget} ->
-        Phoenix.PubSub.broadcast(
-          SocialWidgets.PubSub,
-          "widgets",
-          {:widget_created, widget}
-        )
+  def handle_event("update_poll_question", %{"value" => question}, socket) do
+    {:noreply, assign(socket, :poll_question, question)}
+  end
 
-        {:noreply,
-         socket
-         |> put_flash(:info, "Widget created successfully!")
-         |> assign(:show_form, false)
-         |> assign(:widget_form, to_form(Widgets.change_widget(%Widgets.Widget{})))
-         |> assign(:widgets, Widgets.list_widgets())}
+  @impl true
+  def handle_event("update_poll_option", %{"index" => index, "value" => value}, socket) do
+    index = String.to_integer(index)
+    poll_options = List.replace_at(socket.assigns.poll_options, index, value)
+    {:noreply, assign(socket, :poll_options, poll_options)}
+  end
+
+  @impl true
+  def handle_event("add_poll_option", _params, socket) do
+    poll_options = socket.assigns.poll_options ++ [""]
+    {:noreply, assign(socket, :poll_options, poll_options)}
+  end
+
+  @impl true
+  def handle_event("remove_poll_option", %{"index" => index}, socket) do
+    index = String.to_integer(index)
+    poll_options = List.delete_at(socket.assigns.poll_options, index)
+    {:noreply, assign(socket, :poll_options, poll_options)}
+  end
+
+  @impl true
+  def handle_event("save", %{"widget" => widget_params}, socket) do
+    widget_type = Map.get(widget_params, "widget_type", "poll")
+
+    widget_params_with_config =
+      if widget_type == "poll" do
+        options = Enum.filter(socket.assigns.poll_options, &(&1 != ""))
+
+        if socket.assigns.poll_question == "" || length(options) < 2 do
+          changeset =
+            %Widgets.Widget{}
+            |> Widgets.Widget.changeset(widget_params)
+            |> Ecto.Changeset.add_error(
+              :config,
+              "Poll must have a question and at least 2 options"
+            )
+
+          {:error, changeset}
+        else
+          config = %{
+            "question" => socket.assigns.poll_question,
+            "options" => options
+          }
+
+          {:ok, Map.put(widget_params, "config", config)}
+        end
+      else
+        {:ok, widget_params}
+      end
+
+    case widget_params_with_config do
+      {:ok, params} ->
+        case Widgets.create_widget(params) do
+          {:ok, widget} ->
+            # Create poll options if it's a poll widget
+            if widget.widget_type == "poll" do
+              option_texts = widget.config["options"]
+              Polls.create_poll_options(widget, option_texts)
+            end
+
+            Phoenix.PubSub.broadcast(
+              SocialWidgets.PubSub,
+              "widgets",
+              {:widget_created, widget}
+            )
+
+            {:noreply,
+             socket
+             |> put_flash(:info, "Widget created successfully!")
+             |> assign(:show_form, false)
+             |> assign(:widget_form, to_form(Widgets.change_widget(%Widgets.Widget{})))
+             |> assign(:poll_question, "")
+             |> assign(:poll_options, ["", "", "", ""])
+             |> assign(:widgets, Widgets.list_widgets())}
+
+          {:error, changeset} ->
+            {:noreply, assign(socket, :widget_form, to_form(changeset))}
+        end
 
       {:error, changeset} ->
         {:noreply, assign(socket, :widget_form, to_form(changeset))}
