d88651d8cc3c7dc4d1cd6a148e00f8873a450491
Fly Dev <dev@fly.io>
2026-03-04 11:05:47 +0000

    create Widget schema

---
+++ lib/social_widgets/widgets/widget.ex
@@ -0,0 +1,35 @@
+defmodule SocialWidgets.Widgets.Widget do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "widgets" do
+    field :name, :string
+    field :widget_type, :string
+    field :embed_code, :string
+    field :config, :map
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(widget, attrs) do
+    widget
+    |> cast(attrs, [:name, :widget_type, :embed_code, :config])
+    |> validate_required([:name, :widget_type])
+    |> put_embed_code()
+    |> unique_constraint(:embed_code)
+  end
+
+  defp put_embed_code(changeset) do
+    if get_field(changeset, :embed_code) do
+      changeset
+    else
+      embed_code =
+        :crypto.strong_rand_bytes(8)
+        |> Base.url_encode64(padding: false)
+        |> binary_part(0, 12)
+
+      put_change(changeset, :embed_code, embed_code)
+    end
+  end
+end
