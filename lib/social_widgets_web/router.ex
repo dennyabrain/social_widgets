e541cf78f6f6eed9f7dd2e8295be3037b10ae9dd
Fly Dev <dev@fly.io>
2026-03-04 11:15:31 +0000

    add embed route for poll widgets

--- lib/social_widgets_web/router.ex
+++ lib/social_widgets_web/router.ex
@@ -18,6 +18,8 @@ defmodule SocialWidgetsWeb.Router do
     pipe_through :browser
 
     live "/", DashboardLive
+
+    live "/embed/:embed_code", PollWidgetLive
   end
 
   # Other scopes may use custom stacks.
