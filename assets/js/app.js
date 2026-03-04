84ba3e35150c9356cdbca66869b2c8e0993b6de4
Fly Dev <dev@fly.io>
2026-03-04 11:46:59 +0000

    fix: add WhiteboardCanvas hook to LiveSocket

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,7 +32,7 @@ const csrfToken = document
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
-  hooks: { ...colocatedHooks },
+  hooks: { ...colocatedHooks, WhiteboardCanvas },
 });
 
 // Show progress bar on live navigation and form submits
